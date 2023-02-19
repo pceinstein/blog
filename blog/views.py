dea21c3643261bcbb44df07e0b4c430d9ae65f06
pceinstein <pceinstein@hotmail.com>
2023-02-19 19:34:55 -0300

    Acrescentando Paginação

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 # obter todas as postagens cujo status seja published
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)   # 3 postagens em cada página
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # se a página não for um inteiro, exibe a primeira página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # se a página estiver fora do intervalo,
+        # exibe a última página de resultados
+        posts = paginator.page(paginator.num_pages)
     
     return render(request,
         'blog/post/list.html',
-        {'posts': posts})
+        {'page':page, 'posts': posts})
 
 
 # mostrar uma única postagem
