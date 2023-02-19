9166a801cb13a0db553e1edd783950c9deca4068
pceinstein <pceinstein@hotmail.com>
2023-02-19 20:37:08 -0300

    Usando Views Baseadas em Classe

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
+
+# esta classe substutui a função post_list abaixo
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 # obter todas as postagens cujo status seja published
 def post_list(request):
